96f8130073f2e887c5981ac1fd24cbc712db278a
jerodestapa <jerodestapa@gmail.com>
2016-06-06 23:25:36 -0500

    Re-added config and views

---
+++ project/_config.py
@@ -0,0 +1,15 @@
+# Flask config file
+
+import os
+
+# grab the folder where the script lives
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'flasktaskr.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'my_precious'
+
+# define the full path for the database
+DATABASE_PATH = os.path.join(basedir, DATABASE)
